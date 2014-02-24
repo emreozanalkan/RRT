c26cb3f6201abe8ce29c3cfe205e37a50c76af57
Emre Ozan Alkan <emreozanalkan@gmail.com>
2014-02-24 02:15:47 +0100

    Path
    Finding solution path is added

--- rrt.m
+++ rrt.m
@@ -249,7 +249,22 @@ end
 
 function [path] = fillSolutionPath(edges, vertices)
 
-    path = [0, 0];
+    path = edges(end, 1);
+    
+    prev = edges(end, 2);
+    
+    ii = 0;
+    [edgesCount, ~] = size(edges);
+    
+    while prev ~= 1
+        if ii > edgesCount
+            error('RRT: no path found :(');
+        end
+        prevIndex = find(edges(:, 1) == prev);
+        prev = edges(prevIndex(1), 2);
+        path = [path, prev];
+        ii = ii + 1;
+    end
 
 end
 
